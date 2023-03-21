93df6dc20f486c2b85675ac4625b21a153ac291f
OussamaBaOu <blondblynd@gmail.com>
2023-03-21 12:21:10 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,10 @@
  */
 
 int print_last_digit(int n);
-int r;
 {
-	{
-	if (n < 0)
+int r;
+
+if (n < 0)
 	{
 		n = -n;
 	r = n % 10;
@@ -20,5 +20,4 @@ int r;
 	_putchar(r + '0');
 	}
 	return (r);
-	}
 }
