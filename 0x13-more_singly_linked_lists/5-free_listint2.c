34ff4ac020eead70d57c202b8cf053a132a8f026
OussamaBaOu <blondblynd@gmail.com>
2023-04-29 18:05:25 -0700

    free2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_listint - frees a listint_t list
+ * free_listint2 - frees a listint_t list
  * @head: listint_t list to be freed
  */
 void free_listint2(listint_t **head)
