46cbd59ed1ee531ffb87f8f875d568a5cf6e0c3b
OussamaBaOu <blondblynd@gmail.com>
2023-04-30 07:55:17 -0700

    pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
 /**
  * pop_listint - deletes the head node of a linked list
  * @head: pointer to the first element in the linked list
- * Return - 0 if the list is empty
+ * Return: 0 if the list is empty
  */
 int pop_listint(listint_t **head)
 {
