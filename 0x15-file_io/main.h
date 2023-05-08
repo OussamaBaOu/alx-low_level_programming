f0f8d62a80befcf419e55765f50383d066577076
O.B.O <125463724+OussamaBaOu@users.noreply.github.com>
2023-05-09 00:06:05 +0200

    h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,7 +13,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int cp_file(int argc, char *argv[]);
-char *create_buff(char *file);
-void close_f(int fd);
 
 #endif
