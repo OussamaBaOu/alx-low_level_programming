a453da85dc02796c4dee0b0c890e54db482f195e
O.B.O <125463724+OussamaBaOu@users.noreply.github.com>
2023-07-08 03:14:33 +0200

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,8 +1,10 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_delete - deletes a hash table.
  * @ht: pointer to hash table.
  */
+
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *a = ht;
